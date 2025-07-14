5bb994cc711065b70f1a0b5cab93049f2ec83750
MACHINE\vijay <vijaymanral5@gmail.com>
2025-07-14 13:13:01 +0530

    Changes for Route , Swiper removed from packages

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -6,6 +6,7 @@ import { TrainersComponent } from './static_app/trainers/trainers.component';
 import { EventsComponent } from './static_app/events/events.component';
 import { PricingComponent } from './static_app/pricing/pricing.component';
 import { ContactusComponent } from './static_app/contactus/contactus.component';
+//import { TablePrimeComponent } from './primengtutorial/table-prime/table-prime.component';
 
 export const routes: Routes = [
     { path: 'about', component: AboutComponent },
@@ -14,6 +15,7 @@ export const routes: Routes = [
     { path: 'events', component: EventsComponent },
     { path: 'pricing', component: PricingComponent },
     { path: 'contactus', component: ContactusComponent },
+    // { path: 'prime', component: TablePrimeComponent },
 
     { path: '', component: App }
 ];
--- src/app/app.ts
+++ src/app/app.ts
@@ -3,13 +3,39 @@ import { AppHeaderComponent } from './navigation/header/app-header.component';
 import { FooterComponent } from './navigation/footer/footer-component/footer-component';
 import { Scrolltop } from "./navigation/scrolltop/scrolltop/scrolltop";
 import { RendererComponent } from "./navigation/renderer/renderer.component";
-
+import { NavigationEnd, Router, RouterModule } from '@angular/router';
+import { BehaviorSubject, filter, Observable, Subject, takeUntil } from 'rxjs';
+import { CommonModule } from '@angular/common';
+import { MainsComponent } from "./static_app/main/mains-component/mains-component";
 @Component({
   selector: 'app-root',
-  imports: [AppHeaderComponent, FooterComponent, Scrolltop, RendererComponent],
+  imports: [CommonModule, AppHeaderComponent, FooterComponent, Scrolltop, RouterModule, MainsComponent],
   templateUrl: './app.html',
   styleUrl: './app.css',
 })
 export class App {
   protected title = 'SchoolManagementSystem';
+  private _currentPageSubject = new BehaviorSubject<string | null>('home'); // Initialize with 'home'
+    currentPage$: Observable<string | null> = this._currentPageSubject.asObservable();
+  
+    private _destroy$ = new Subject<void>(); // Subject to signal component destruction
+  
+    constructor(private router: Router) { }
+  
+    ngOnInit(): void {
+      this.router.events.pipe(
+        filter(event => event instanceof NavigationEnd),
+        takeUntil(this._destroy$) // Automatically unsubscribe when _destroy$ emits
+      ).subscribe((event: NavigationEnd) => {
+        const url = event.urlAfterRedirects;
+        let page = url.split('/').filter(Boolean).pop() || null;
+        page = page === null ? 'home' : page;
+        this._currentPageSubject.next(page); // Emit the new page value
+      });
+    }
+  
+    ngOnDestroy(): void {
+      this._destroy$.next();
+      this._destroy$.complete();
+    }
 }
